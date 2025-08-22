ad5d5612963fb8b1bb3f01c6676c57c2a5fd0520
tkderphu <quangphu.re050204@gmail.com>
2025-08-22 21:54:04 +0700

    fix

---
+++ game/src/main/java/site/viosmash/game/Game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package site.viosmash.game;
+
+/**
+ *
+ * @author FPT
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
