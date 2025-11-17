281d91346611fd89f1233b7a2ea668adbe2c4c51
tkderphu <quangphu.re050204@gmail.com>
2025-11-17 21:41:12 +0700

    final

--- server/src/main/java/site/viosmash/server/dao/Dao.java
+++ server/src/main/java/site/viosmash/server/dao/Dao.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Dao {
     private static String url = "jdbc:mysql://localhost:3306/cgo?useSSL=false&serverTimezone=UTC";
     private static String user = "root";
-    private static String pass = "";
+    private static String pass = "root";
 
     protected Connection conn;
 
